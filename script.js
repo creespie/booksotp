036db2641bef3c420d537558336cbb51ff83f365
Ludovico rossi <ludovico@MacBook-Pro-di-Ludovico.local>
2025-09-13 00:08:19 +0200

    add first exercise constructor

---
+++ script.js
@@ -0,0 +1,14 @@
+function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function(){
+       info = title + ", by " + author + ", " + pages + " pages, " + (read ? "already read" : "not read yet");
+       return info;
+    }
+}
+
+var theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, 0);
+
+console.log(theHobbit.info()); // "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
