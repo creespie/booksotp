cce98498c2fb66685ccf8749c11f816f35f28404
Ludovico rossi <ludovico@MacBook-Pro-di-Ludovico.local>
2025-09-13 17:54:02 +0200

    add books display and add functions

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-const myLibrary = [];
+const myLibrary = [
+    { title: "1984", author: "George Orwell", pages: 328, read: true, idBook: 1 },
+    { title: "Il Signore degli Anelli", author: "J.R.R. Tolkien", pages: 1178, read: false, idBook: 2 }
+];
 
 function Book(title, author, pages, read){
-
+    //check for new
     if (!new.target) {
         throw Error('Must use the new operator to call the constructor');
     }
-
+    //book attributes
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -15,13 +18,81 @@ function Book(title, author, pages, read){
        info = title + ", by " + author + ", " + pages + " pages, " + (read ? "already read, " : "not read yet, ") + id;
        return info;
     };
+    //adds to the array
+    myLibrary.push(this)
 }
 
-function addBook(){
+//library container in the html
+
+const library = document.getElementById("library");
+
+function displayBooks(){
+    //clear old layout
+    let count = document.getElementById("library").children.length;
+
+    while(count > 0){
+        const book = document.getElementsByClassName("book")[0];
+        const title = document.getElementsByClassName("title")[0];
+        book.removeChild(title);
+        const author = document.getElementsByClassName("author")[0];
+        book.removeChild(author);
+        const pages = document.getElementsByClassName("pages")[0];
+        book.removeChild(pages);
+        const read = document.getElementsByClassName("read")[0];
+        book.removeChild(read);
+        const idBook = document.getElementsByClassName("idBook")[0];
+        book.removeChild(idBook);
+        
+        library.removeChild(book);
+        count--;
+    };
+    //create space for the book
+    for(let item of myLibrary) {
     
-}
+    const book = document.createElement("div");
+    book.classList.add("book");
+    library.appendChild(book);
 
+    const title = document.createElement("div");
+    title.classList.add("title");
+    book.appendChild(title);
+    title.textContent = item.title;
 
-var theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, 0);
+    const author = document.createElement("div");
+    author.classList.add("author");
+    book.appendChild(author);
+    author.textContent = item.author;
 
-console.log(theHobbit.info()); // "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
+    const pages = document.createElement("div");
+    pages.classList.add("pages");
+    book.appendChild(pages);
+    pages.textContent = item.pages;
+
+    const read = document.createElement("div");
+    read.classList.add("read");
+    book.appendChild(read);
+    read.textContent = item.read ? "already read" : "not read yet";
+
+    const idBook = document.createElement("div");
+    idBook.classList.add("idBook");
+    book.appendChild(idBook);
+    idBook.textContent = item.id;
+    }
+};
+
+//book creator
+const button = document.getElementById("submit");
+button.addEventListener("click", () => {
+    title = document.getElementById("title").value;
+    author = document.getElementById("author").value;
+    pages = document.getElementById("pages").value;
+    read = document.getElementById("read").checked;
+    
+    new Book(title, author, pages, read );
+    displayBooks();
+  });
+
+  displayBooks();
+//var theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, 0);
+
+//console.log(theHobbit.info());  "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
