a6b0e1eea38046c0913f060dc0ad68635da487c5
Ludovico rossi <ludovico@MacBook-Pro-di-Ludovico.local>
2025-09-13 16:24:42 +0200

    add base library array and id generator

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
+const myLibrary = [];
+
 function Book(title, author, pages, read){
+
+    if (!new.target) {
+        throw Error('Must use the new operator to call the constructor');
+    }
+
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.id = crypto.randomUUID();
     this.info = function(){
-       info = title + ", by " + author + ", " + pages + " pages, " + (read ? "already read" : "not read yet");
+       info = title + ", by " + author + ", " + pages + " pages, " + (read ? "already read, " : "not read yet, ") + id;
        return info;
-    }
+    };
 }
+
+function addBook(){
+    
+}
+
 
 var theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, 0);
 
